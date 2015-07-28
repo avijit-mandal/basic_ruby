f52538c5071b2f2df73de7aaf1faacc096d08890
Avijit Mandal <avijit@codeapogee.com>
2015-07-28 12:07:23 +0530

    check for balance.txt

--- ATM/prj_test.rb
+++ ATM/prj_test.rb
@@ -21,22 +21,18 @@ def test(entry)
 		
 	elsif entry=="B" || entry=="b"
 		
-		File.open("#{@account_no}balance", "r") do |aFile|
-		print while aFile.gets
-		end
+		balance = File.open("#{@account_no}balance", &:gets)
+		puts "\tYour account balance is Rs.#{balance}"
 	
 	elsif entry == 'Q'  || entry=="q"
 		puts "\tThank you for using our service"
-		File.open("#{@account_no}balance", "w") do |aFile|
-		aFile.puts "#{total}"
-		end
+#		
 	else
 		puts "\tEnter a correct key D, W, B or Q"
 	end
 end
 
 def user_input
-	
 	print "\t D)deposit W)ithdraw B)alance Q)uite :  "
 	
 	choice = gets
@@ -46,10 +42,23 @@ def user_input
 	user_input
 end
 
+def text_ex
+	if File.exist?("#{@account_no}balance")
+		user_input
+	else
+				
+		File.open("#{@account_no}balance", "w") do |aFile|
+		aFile.puts "100"
+		end
+		user_input
+	end
+	
+end
 
+	
 
 end
 
 a=Account.new(100)
-puts a.user_input
+puts a.text_ex
 	
