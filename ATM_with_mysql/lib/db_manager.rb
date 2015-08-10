27402ade34f1b319360499b199436fd92c161c85
Avijit Mandal <avijit@codeapogee.com>
2015-08-10 17:21:47 +0530

    code refactoring

--- ATM_with_mysql/lib/db_manager.rb
+++ ATM_with_mysql/lib/db_manager.rb
@@ -1,44 +1,39 @@
 
 require 'mysql2'
-require 'pathname'
 
 module Utility
 	module DB_Manager
 	  extend self
-	  attr_reader :account
+	  #~ attr_reader :account
 	  
-	  def client
-      Mysql2::Client.new(:host => "localhost", :username => "root", :database => 'atm')
-    end
+		def client
+			Mysql2::Client.new(:host => "localhost", :username => "root", :database => 'atm')
+		end
     
     
-	  def create_balance_db(account)
-      client.query("SELECT balance FROM accounts WHERE account_no = '#{account}'").each do |row|
-        @row=row
-      end
+		def create_balance_db(account)
+			res = client.query("SELECT balance FROM accounts WHERE account_no = '#{account}'")
 		
-      if !@row
-        client.query("INSERT INTO accounts VALUES (#{account}, 100);")
-      end
-	    
-	  end
-	 
-	  
-	  def read(account_no)
-      
-      res = client.query("SELECT balance FROM accounts WHERE account_no = '#{account_no}'")
-      result=res.map{|row| row['balance']}
-      @current_balance = result[0]
-      
-      return @current_balance
-	  
-    end
-	  
-	  def write(account_no, amount)
+			if res.count.zero?
+				client.query("INSERT INTO accounts VALUES (#{account}, 100);")
 
-      client.query("UPDATE accounts SET balance = '#{amount}' WHERE account_no = '#{account_no}' ")
+			end
 	    
-	  end
+		end
+	  
+		def read(account_no)
+      
+			res = client.query("SELECT balance FROM accounts WHERE account_no = '#{account_no}'")
+			result=res.map{|row| row['balance']}
+			result[0]
+	  
+		end
+	  
+		def write(account_no, amount)
+
+			client.query("UPDATE accounts SET balance = '#{amount}' WHERE account_no = '#{account_no}' ")
+	    
+		end
 
 
 	end
